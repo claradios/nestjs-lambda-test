190d94f8596fe21ced5c40daabcb6634df143d7e
Clara Dios <clara.dios@guidesmiths.com>
2022-02-07 10:39:03 +0100

    test: add simple cats app test

--- test/CatsApp.e2e-spec.ts
+++ test/CatsApp.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { CatsModule } from './../src/cats/cats.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [CatsModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -17,8 +17,11 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/cats/all')
       .expect(200)
-      .expect('Hello World!');
+      .expect([
+        { catName: 'Rosina', age: 12 },
+        { catName: 'Cati Peluso', age: 50 },
+      ]);
   });
 });
