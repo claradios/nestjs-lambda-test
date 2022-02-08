568bc746eaa0708d8626021600983895de33d301
Clara Dios <clara.dios@guidesmiths.com>
2022-02-08 10:32:25 +0100

    ci: test

--- src/dogs/domain/services/dogs/dogs.service.ts
+++ src/dogs/domain/services/dogs/dogs.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class DogsService {
   getHello(): string {
-    return 'Hello World! Welcome to the DOGS service';
+    return 'Hello World!!! Welcome to the DOGS service.';
   }
 
   getAll(): Array<object> {
